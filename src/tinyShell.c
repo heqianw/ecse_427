32a788072ab2c7ff363dc750228c1ad3cfcfcdb4
heqian <heqianw@gmail.com>
2018-09-24 15:41:40 -0400

    Done fork()

--- src/tinyShell.c
+++ src/tinyShell.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 
 int my_system(char *command){
-    return -1;
+    char* argv[50];
+    char *list = strtok(command, " ");
+    char* path = "/bin/";
+    char progpath[20];
+    int i = 0;
+    while(list != NULL && i < 50){
+        argv[i] = list;
+        list = strtok(NULL, " ");
+        i++;
+    }
+    argv[i] = NULL;
+    int argCount = i;
+    for(i = 0; i < argCount; i++){
+        printf("%s\n", argv[i]);
+    }
+    strcpy(progpath, path);
+    strcat(progpath, argv[0]);
+
+    for(i = 0; i < strlen(progpath); i++){
+        if(progpath[i] == '\n'){
+            progpath[i] = '\0';
+        }
+    }
+    
+    int pid = fork();
+    if(pid == 0){
+        execvp(progpath, argv);
+    }
+    return 0;
 }
 
 int length(char *s){
@@ -25,7 +54,7 @@ int main(int argc, char *argv[]){
     while(1){
         char *line = get_a_line();
         if (length(line) > 1)
-            my_system(line);
+            system(line);
         else    
             return -1;
     }
