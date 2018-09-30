eb6b661b36005249ccadcd321c5ae3244cf02f2b
He Qian Wang <heqianw@gmail.com>
2018-09-29 21:02:00 -0400

    Added temporary files

--- src/tinyShell.c
+++ src/tinyShell.c
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define _GNU_SOURCE
+#include <sched.h>
+#include <signal.h>
 
 int my_systemf(char *command){
     char* argv[50];
@@ -74,9 +77,66 @@ int my_systemv(char *command){
     }
 }
 
+int fn(void *arg)
+{
+   printf("\nINFO: This code is running under child process.\n");
+
+   int i = 0;
+   
+   int n = atoi(arg);
+
+   for ( i = 1 ; i <= 10 ; i++ )
+      printf("%d * %d = %d\n", n, i, (n*i));
+
+   printf("\n");
+
+   return 0;
+}
+
 // this one uses clone
 int my_systemc(char *command){
+    char* argv[50];
+    char *list = strtok(command, " ");
+    char* path = "/bin/";
+    char progpath[20];
+    int i = 0;
+    while(list != NULL && i < 50){
+        argv[i] = list;
+        list = strtok(NULL, " ");
+        i++;
+    }
+    argv[i] = NULL;
+    int argCount = i;
+    // for(i = 0; i < argCount; i++){
+    //     printf("%s\n", argv[i]);
+    // }
+    strcpy(progpath, path);
+    strcat(progpath, argv[0]);
 
+    for(i = 0; i < strlen(progpath); i++){
+        if(progpath[i] == '\n'){
+            progpath[i] = '\0';
+        }
+    }
+    //here, we need to create a clone process
+
+    printf("Hello, World!\n");
+
+    void *pchild_stack = malloc(1024 * 1024);
+    if ( pchild_stack == NULL ) {
+        printf("ERROR: Unable to allocate memory.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int pid = clone(fn, pchild_stack + (1024 * 1024), SIGCHLD, argv[1]);
+    if(pid == 0){
+        execvp(progpath, argv);
+    }
+    else{
+        wait(NULL);
+    }
+
+    free(pchild_stack);
 }
 
 int length(char *s){
@@ -98,7 +158,7 @@ int main(int argc, char *argv[]){
         char *line = get_a_line();
         if (length(line) > 1)
             //my_systemf(line);
-            my_systemv(line);
+            my_systemc(line);
         else    
             return -1;
     }
