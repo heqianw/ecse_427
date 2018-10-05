b6b3995018187aa52f213f933b9754c5f11526ed
heqian <heqianw@gmail.com>
2018-10-05 00:05:46 -0400

    Final version

---
+++ src/tiny_shell.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define _GNU_SOURCE
+#include <sched.h>
+#include <signal.h>
+
+int length(char *s){
+    int x = 0;
+    while(s[x] != '\0')
+        x++;
+    return x;
+} 
+
+char *get_a_line(){
+    char *line = NULL;
+    size_t bufsize = 0;
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+int main(int argc, char *argv[]){
+    while(1){
+        char *line = get_a_line();
+        if (length(line) > 1)
+            my_systemf(line);
+        else    
+            return -1;
+    }
+}
