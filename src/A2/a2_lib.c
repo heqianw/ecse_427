ec5202627a6912dcb80dee47206d57ff38f474a7
He Qian Wang <heqianw@gmail.com>
2018-11-05 16:01:46 -0500

    Submitted assignment, added comments

--- src/A2/a2_lib.c
+++ src/A2/a2_lib.c
@@ -17,6 +17,7 @@ sem_t *my_sem_read;
 kv_info *this_kv_info;
 char *shmname;
 
+//hash function that is given, compute a hash of a string
 unsigned long hash(unsigned char *str){
     unsigned long hash = 0;
     int c;
@@ -26,6 +27,7 @@ unsigned long hash(unsigned char *str){
     return (hash > 0) ? hash : -(hash);
 }
 
+//method to create the kv_store
 int kv_store_create(char *name){
     // create the kv at the designated name
     int fd = shm_open(__KV_SHM_NAME__, O_CREAT | O_RDWR, S_IRWXU);
@@ -35,17 +37,21 @@ int kv_store_create(char *name){
        return -1;
     }
     shmname = name;
+    //map the location of the shared memory
     this_kv_info = mmap(NULL, sizeof(kv_info), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     ftruncate(fd, sizeof(kv_info));
-    // memcpy(addr, str, strlen(str));
     this_kv_info -> kv_name;
+    // after doing stuff dereference the shared memory
     munmap(this_kv_info, sizeof(kv_info));
     close(fd);
     return 0;
 }
 
+
+// method to write to the kv_store
 int kv_store_write(char *key, char *value){
 
+    // truncate the string with a null value to avoid issues later on
     if(strlen(key) > 32){
         *(key + 32) = '\0';
     }
@@ -55,25 +61,29 @@ int kv_store_write(char *key, char *value){
 
     int i;
     int keyExists = 0;
+    // open the shared memory
     int fd = shm_open(__KV_SHM_NAME__, O_RDWR, S_IRWXU);
     if (fd < 0){
        printf("Failed to write to KV \n");
        return -1;
     }
 
+    // reference the shared memory
     this_kv_info = mmap(NULL, sizeof(kv_info), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     ftruncate(fd, sizeof(kv_info)+sizeof(char));
     
+    // set up the write semaphore and obtain a lock 
     my_sem_write = sem_open(__KV_WRITERS_SEMAPHORE__,O_CREAT | O_RDWR, S_IRWXU, 1);
     sem_wait(my_sem_write);
     
-    //this is the critical section
+    // this is the start of critical section
+    // identify which pod that  contains the kv-pair
     int podIndex = hash(key) % NUMBERPODS;
     pod *actualPod = & (this_kv_info -> pods[podIndex]);
     int actualIndex = (actualPod -> index);
 
     kv_pair *actualPair = NULL;
-    //have to check if key actually exists
+    // have to check if key already exists inside this pod
     for(i = 0; i < actualIndex; i++){
         kv_pair *currentPair = &(actualPod -> kv_pairs[i]);
         char *storedKey = (currentPair -> key);
@@ -83,27 +93,32 @@ int kv_store_write(char *key, char *value){
         }
     }
 
-    //if we cant the existing key within the pod; then 
+    // if we cant find the existing key within the pod
+    // create a new kv-pair inside the pod
     if(keyExists == 0)
         actualPair = & (actualPod -> kv_pairs[actualIndex]);
-  
+    // figure out which position to write to based on the writeIndex
     int writeIndex = (actualPair -> writeIndex);
     int numberValues = (actualPair -> numberValues);
 
+    //if number of values is too high, use FIFO
     if(numberValues <= MAXNUMBERVALUES){
         numberValues++;
         actualPair -> numberValues = numberValues;
     }
     
+    //write to the position in memory
     memcpy(&actualPair -> key, key, strlen(key));
     memcpy(&actualPair -> value[writeIndex], value , strlen(value) + 1);
 
+    //increment the writeindex
     writeIndex++;
     if(writeIndex >= MAXNUMBERVALUES)
         writeIndex = writeIndex % MAXNUMBERVALUES;
 
     actualPair -> writeIndex = writeIndex;
 
+    //if create a new key, then increment number of kv-pairs
     if(keyExists == 0){
         actualIndex++;
     }
@@ -114,8 +129,9 @@ int kv_store_write(char *key, char *value){
         actualPod -> index = actualIndex;
     }
 
+    //release the semaphore and unlink semaphore/shared memory
     sem_post(my_sem_write);
-    sem_close(my_sem_write);
+    sem_close(my_sem_write); 
     munmap(this_kv_info, sizeof(kv_info));
     close(fd);
     return 0;
@@ -123,38 +139,42 @@ int kv_store_write(char *key, char *value){
 
 //once we compute hash and we get to the hash, parse the list of values for the key that we want, by doing key equality
 char *kv_store_read(char *key){
+    //open the shared memory
     int fd = shm_open(__KV_SHM_NAME__, O_RDWR, S_IRWXU);
     if (fd < 0){
        printf("Failed to write to KV \n");
        return NULL;
     }
-
+    //map the location of shared mem
     this_kv_info = mmap(NULL, sizeof(kv_info), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     ftruncate(fd, sizeof(kv_info) + sizeof(char));
     int i;
     char *valueToReturn;
     int found = 0;
-
-   
+    //obtain lock on read
     my_sem_read = sem_open(__KV_READERS_SEMAPHORE__,O_CREAT | O_RDWR, S_IRWXU, 1);
     my_sem_write = sem_open(__KV_WRITERS_SEMAPHORE__,O_CREAT | O_RDWR, S_IRWXU, 1);
     sem_wait(my_sem_read);
-
+    //if we can read, get lock on write
     (this_kv_info -> readCount)++;
     if((this_kv_info -> readCount) == 1){
         sem_wait(my_sem_write);
     }
     sem_post(my_sem_read);
 
+    //figure out which pod to read from
     int podIndex = hash(key) % NUMBERPODS;
     pod *actualPod = & (this_kv_info -> pods[podIndex]);
     int numberEntries = (actualPod -> index);
     
+    //check every kv-pair to see which key we can read
     for(i = 0; i < numberEntries; i++){
         kv_pair *currentPair = &(actualPod -> kv_pairs[i]);
         char *storedKey = (currentPair -> key);
         if(strcmp(key, storedKey) == 0){
+            //based on the readIndex, read the approriate value
             int readIndex = (currentPair -> readIndex);
+            //duplicate that string
             valueToReturn = strdup((currentPair -> value[readIndex]));
             found = 1;
             readIndex++;
@@ -164,20 +184,22 @@ char *kv_store_read(char *key){
             currentPair -> readIndex = readIndex;
         }
     }
-
+    //get a lock on read
     sem_wait(my_sem_read);
     (this_kv_info -> readCount)--;
     
+    //release the write lock
     if((this_kv_info -> readCount) == 0){
         sem_post(my_sem_write);
     }
+    //dereference everything
     sem_post(my_sem_read);
     sem_close(my_sem_read);
     sem_close(my_sem_write);
 
     munmap(this_kv_info, sizeof(kv_info));
     close(fd);
-    
+    //if found value, return; else return NULL
     if(found == 1){
         return valueToReturn;
     }
@@ -185,10 +207,11 @@ char *kv_store_read(char *key){
         return NULL;
     }
 }
-
+// read all values of a specific key
 char ** kv_store_read_all(char *key){
+    //allocate enough memory for the array of pointers
 	char** allValues = calloc((MAXNUMBERVALUES + 1), sizeof(char*));
-
+    //link to shared memory
 	int fd = shm_open(__KV_SHM_NAME__, O_RDWR, S_IRWXU);
     if (fd < 0){
        printf("Failed to write to KV \n");
@@ -200,7 +223,7 @@ char ** kv_store_read_all(char *key){
     int i;
     int j;
     int found = 0;
-
+    //get a lock on read so that we can get a lock on write
     my_sem_read = sem_open(__KV_READERS_SEMAPHORE__,O_CREAT | O_RDWR, S_IRWXU, 1);
     my_sem_write = sem_open(__KV_WRITERS_SEMAPHORE__,O_CREAT | O_RDWR, S_IRWXU, 1);
     sem_wait(my_sem_read);
@@ -211,14 +234,17 @@ char ** kv_store_read_all(char *key){
     }
     sem_post(my_sem_read);
 
+    //figure out which pod to read from
     int podIndex = hash(key) % NUMBERPODS;
     pod *actualPod = & (this_kv_info -> pods[podIndex]);
     int numberEntries = (actualPod -> index);
     
+    //find if key matches which existing ones
     for(i = 0; i < numberEntries; i++){
         kv_pair *currentPair = &(actualPod -> kv_pairs[i]);
         char *storedKey = (currentPair -> key);
         if(strcmp(key, storedKey) == 0){
+            //if found, read all values and duplicate into the array of chars
             for(j = 0; j < (currentPair -> numberValues); j++){
                 allValues[j] = strdup((currentPair -> value[j]));
             }
@@ -226,20 +252,21 @@ char ** kv_store_read_all(char *key){
             break;
         }
     }
-
+    //release lock on read and lock on write
     sem_wait(my_sem_read);
     (this_kv_info -> readCount)--;
     
     if((this_kv_info -> readCount) == 0){
         sem_post(my_sem_write);
     }
+    //dereference everything
     sem_post(my_sem_read);
     sem_close(my_sem_read);
     sem_close(my_sem_write);
 
     munmap(this_kv_info, sizeof(kv_info));
     close(fd);
-    
+    //return if found
     if(found == 1){
         return allValues;
     }
